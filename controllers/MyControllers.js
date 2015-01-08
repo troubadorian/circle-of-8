e2d29fd9095892be585dfadfe425ab72f6991ca4
Deveedutta Maharana <deveedutta@gmail.com>
2015-01-09 04:37:55 +0530

    have to go to office in time tomorrow: good night

--- controllers/MyControllers.js
+++ controllers/MyControllers.js
@@ -1,20 +1,20 @@
 var MC = {};
-
-function MyRootViewController (request, response, query, hash) {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("<html><head></head><body><script src='main.js'></script><h1>I'm at the root view</h1></body></html>");
-	response.end();
-}
-MC.MyRootViewController = MyRootViewController;
-
-
-
+//function carry the same name as defined in Routes
 MC.MyFaviconViewController = function (request, response, query, hash) {
 	response.writeHead(200, {"Content-Type": "image/png"});
 	response.end();
 }
 
-
+//An alternate approach can be the following
+//Define function with any name you like
+//Named methods are always good
+function MyRootViewController_WithCustomName (request, response, query, hash) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<html><head></head><body><script src='main.js'></script><h1>I'm at the root view</h1></body></html>");
+	response.end();
+}
+//Make sure the name matches when exporting
+MC.MyRootViewController = MyRootViewController_WithCustomName;
 
 function MyUserViewController (request, response, query, hash) {
 	response.writeHead(200, {"Content-Type": "application/json"});
